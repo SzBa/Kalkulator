c0621a74a1848c44c0d2fa9249930d31fa41715a
SzBa <48062979+SzBa@users.noreply.github.com>
2019-03-19 11:32:49 +0100

    Moj pierwszy kalkulator okienkowy

---
+++ Program/Program/Form1.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace Program
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pole = new System.Windows.Forms.TextBox();
+            this.b5 = new System.Windows.Forms.Button();
+            this.b7 = new System.Windows.Forms.Button();
+            this.b8 = new System.Windows.Forms.Button();
+            this.b9 = new System.Windows.Forms.Button();
+            this.b4 = new System.Windows.Forms.Button();
+            this.b6 = new System.Windows.Forms.Button();
+            this.b1 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.plus = new System.Windows.Forms.Button();
+            this.minus = new System.Windows.Forms.Button();
+            this.mnozenie = new System.Windows.Forms.Button();
+            this.dzielenie = new System.Windows.Forms.Button();
+            this.rowna = new System.Windows.Forms.Button();
+            this.b0 = new System.Windows.Forms.Button();
+            this.kropka = new System.Windows.Forms.Button();
+            this.wyczysc = new System.Windows.Forms.Button();
+            this.cofnij = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // pole
+            // 
+            this.pole.Location = new System.Drawing.Point(12, 26);
+            this.pole.Name = "pole";
+            this.pole.Size = new System.Drawing.Size(178, 20);
+            this.pole.TabIndex = 0;
+            // 
+            // b5
+            // 
+            this.b5.Location = new System.Drawing.Point(58, 88);
+            this.b5.Name = "b5";
+            this.b5.Size = new System.Drawing.Size(40, 30);
+            this.b5.TabIndex = 5;
+            this.b5.Text = "5";
+            this.b5.UseVisualStyleBackColor = true;
+            this.b5.Click += new System.EventHandler(this.b5_Click);
+            // 
+            // b7
+            // 
+            this.b7.Location = new System.Drawing.Point(12, 52);
+            this.b7.Name = "b7";
+            this.b7.Size = new System.Drawing.Size(40, 30);
+            this.b7.TabIndex = 6;
+            this.b7.Text = "7";
+            this.b7.UseVisualStyleBackColor = true;
+            this.b7.Click += new System.EventHandler(this.b7_Click);
+            // 
+            // b8
+            // 
+            this.b8.Location = new System.Drawing.Point(58, 52);
+            this.b8.Name = "b8";
+            this.b8.Size = new System.Drawing.Size(40, 30);
+            this.b8.TabIndex = 7;
+            this.b8.Text = "8";
+            this.b8.UseVisualStyleBackColor = true;
+            this.b8.Click += new System.EventHandler(this.b8_Click);
+            // 
+            // b9
+            // 
+            this.b9.Location = new System.Drawing.Point(104, 52);
+            this.b9.Name = "b9";
+            this.b9.Size = new System.Drawing.Size(40, 30);
+            this.b9.TabIndex = 8;
+            this.b9.Text = "9";
+            this.b9.UseVisualStyleBackColor = true;
+            this.b9.Click += new System.EventHandler(this.b9_Click);
+            // 
+            // b4
+            // 
+            this.b4.Location = new System.Drawing.Point(12, 88);
+            this.b4.Name = "b4";
+            this.b4.Size = new System.Drawing.Size(40, 30);
+            this.b4.TabIndex = 9;
+            this.b4.Text = "4";
+            this.b4.UseVisualStyleBackColor = true;
+            this.b4.Click += new System.EventHandler(this.b4_Click);
+            // 
+            // b6
+            // 
+            this.b6.Location = new System.Drawing.Point(104, 88);
+            this.b6.Name = "b6";
+            this.b6.Size = new System.Drawing.Size(40, 30);
+            this.b6.TabIndex = 10;
+            this.b6.Text = "6";
+            this.b6.UseVisualStyleBackColor = true;
+            this.b6.Click += new System.EventHandler(this.b6_Click);
+            // 
+            // b1
+            // 
+            this.b1.Location = new System.Drawing.Point(12, 124);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(40, 30);
+            this.b1.TabIndex = 11;
+            this.b1.Text = "1";
+            this.b1.UseVisualStyleBackColor = true;
+            this.b1.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // b2
+            // 
+            this.b2.Location = new System.Drawing.Point(58, 124);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(40, 30);
+            this.b2.TabIndex = 12;
+            this.b2.Text = "2";
+            this.b2.UseVisualStyleBackColor = true;
+            this.b2.Click += new System.EventHandler(this.b2_Click);
+            // 
+            // b3
+            // 
+            this.b3.Location = new System.Drawing.Point(104, 124);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(40, 30);
+            this.b3.TabIndex = 13;
+            this.b3.Text = "3";
+            this.b3.UseVisualStyleBackColor = true;
+            this.b3.Click += new System.EventHandler(this.b3_Click);
+            // 
+            // plus
+            // 
+            this.plus.Location = new System.Drawing.Point(150, 52);
+            this.plus.Name = "plus";
+            this.plus.Size = new System.Drawing.Size(40, 30);
+            this.plus.TabIndex = 14;
+            this.plus.Text = "+";
+            this.plus.UseVisualStyleBackColor = true;
+            this.plus.Click += new System.EventHandler(this.plus_Click);
+            // 
+            // minus
+            // 
+            this.minus.Location = new System.Drawing.Point(150, 88);
+            this.minus.Name = "minus";
+            this.minus.Size = new System.Drawing.Size(40, 30);
+            this.minus.TabIndex = 15;
+            this.minus.Text = "-";
+            this.minus.UseVisualStyleBackColor = true;
+            this.minus.Click += new System.EventHandler(this.minus_Click);
+            // 
+            // mnozenie
+            // 
+            this.mnozenie.Location = new System.Drawing.Point(150, 124);
+            this.mnozenie.Name = "mnozenie";
+            this.mnozenie.Size = new System.Drawing.Size(40, 30);
+            this.mnozenie.TabIndex = 16;
+            this.mnozenie.Text = "*";
+            this.mnozenie.UseVisualStyleBackColor = true;
+            this.mnozenie.Click += new System.EventHandler(this.mnozenie_Click);
+            // 
+            // dzielenie
+            // 
+            this.dzielenie.Location = new System.Drawing.Point(150, 160);
+            this.dzielenie.Name = "dzielenie";
+            this.dzielenie.Size = new System.Drawing.Size(40, 30);
+            this.dzielenie.TabIndex = 17;
+            this.dzielenie.Text = "/";
+            this.dzielenie.UseVisualStyleBackColor = true;
+            this.dzielenie.Click += new System.EventHandler(this.dzielenie_Click);
+            // 
+            // rowna
+            // 
+            this.rowna.Location = new System.Drawing.Point(12, 196);
+            this.rowna.Name = "rowna";
+            this.rowna.Size = new System.Drawing.Size(86, 30);
+            this.rowna.TabIndex = 18;
+            this.rowna.Text = "=";
+            this.rowna.UseVisualStyleBackColor = true;
+            this.rowna.Click += new System.EventHandler(this.rowna_Click);
+            // 
+            // b0
+            // 
+            this.b0.Location = new System.Drawing.Point(58, 160);
+            this.b0.Name = "b0";
+            this.b0.Size = new System.Drawing.Size(40, 30);
+            this.b0.TabIndex = 19;
+            this.b0.Text = "0";
+            this.b0.UseVisualStyleBackColor = true;
+            this.b0.Click += new System.EventHandler(this.b0_Click);
+            // 
+            // kropka
+            // 
+            this.kropka.Location = new System.Drawing.Point(104, 160);
+            this.kropka.Name = "kropka";
+            this.kropka.Size = new System.Drawing.Size(40, 30);
+            this.kropka.TabIndex = 20;
+            this.kropka.Text = ",";
+            this.kropka.UseVisualStyleBackColor = true;
+            this.kropka.Click += new System.EventHandler(this.kropka_Click);
+            // 
+            // wyczysc
+            // 
+            this.wyczysc.Location = new System.Drawing.Point(12, 160);
+            this.wyczysc.Name = "wyczysc";
+            this.wyczysc.Size = new System.Drawing.Size(40, 30);
+            this.wyczysc.TabIndex = 21;
+            this.wyczysc.Text = "C";
+            this.wyczysc.UseVisualStyleBackColor = true;
+            this.wyczysc.Click += new System.EventHandler(this.wyczysc_Click);
+            // 
+            // cofnij
+            // 
+            this.cofnij.Location = new System.Drawing.Point(104, 196);
+            this.cofnij.Name = "cofnij";
+            this.cofnij.Size = new System.Drawing.Size(86, 30);
+            this.cofnij.TabIndex = 22;
+            this.cofnij.Text = "BACKSPACE";
+            this.cofnij.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.cofnij.UseVisualStyleBackColor = true;
+            this.cofnij.Click += new System.EventHandler(this.cofnij_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(273, 267);
+            this.Controls.Add(this.cofnij);
+            this.Controls.Add(this.wyczysc);
+            this.Controls.Add(this.kropka);
+            this.Controls.Add(this.b0);
+            this.Controls.Add(this.rowna);
+            this.Controls.Add(this.dzielenie);
+            this.Controls.Add(this.mnozenie);
+            this.Controls.Add(this.minus);
+            this.Controls.Add(this.plus);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b1);
+            this.Controls.Add(this.b6);
+            this.Controls.Add(this.b4);
+            this.Controls.Add(this.b9);
+            this.Controls.Add(this.b8);
+            this.Controls.Add(this.b7);
+            this.Controls.Add(this.b5);
+            this.Controls.Add(this.pole);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Kalkulator by Babula";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox pole;
+        private System.Windows.Forms.Button b5;
+        private System.Windows.Forms.Button b7;
+        private System.Windows.Forms.Button b8;
+        private System.Windows.Forms.Button b9;
+        private System.Windows.Forms.Button b4;
+        private System.Windows.Forms.Button b6;
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button plus;
+        private System.Windows.Forms.Button minus;
+        private System.Windows.Forms.Button mnozenie;
+        private System.Windows.Forms.Button dzielenie;
+        private System.Windows.Forms.Button rowna;
+        private System.Windows.Forms.Button b0;
+        private System.Windows.Forms.Button kropka;
+        private System.Windows.Forms.Button wyczysc;
+        private System.Windows.Forms.Button cofnij;
+    }
+}
+
---
+++ Program/Program/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        char znak;  
+        double liczba; 
+
+
+        private void plus_Click(object sender, EventArgs e)
+        {
+            liczba = double.Parse(pole.Text);
+            znak = '+';
+            pole.Text = "";
+        }
+
+        private void minus_Click(object sender, EventArgs e)
+        {
+            liczba = double.Parse(pole.Text);
+            znak = '-';
+            pole.Text = "";
+        }
+
+        private void mnozenie_Click(object sender, EventArgs e)
+        {
+            liczba = double.Parse(pole.Text);
+            znak = '*';
+            pole.Text = "";
+        }
+
+        private void dzielenie_Click(object sender, EventArgs e)
+        {
+            liczba = double.Parse(pole.Text);
+            znak = '/';
+            pole.Text = "";
+        }
+
+        private void kropka_Click(object sender, EventArgs e)
+        {
+            pole.AppendText(".");
+        }
+
+        private void b3_Click(object sender, EventArgs e)
+        {
+            pole.AppendText("3");
+        }
+
+        private void b6_Click(object sender, EventArgs e)
+        {
+            pole.AppendText("6");
+        }
+
+        private void b9_Click(object sender, EventArgs e)
+        {
+            pole.AppendText("9");
+        }
+
+        private void b8_Click(object sender, EventArgs e)
+        {
+            pole.AppendText("8");
+        }
+
+        private void b5_Click(object sender, EventArgs e)
+        {
+            pole.AppendText("5");
+        }
+
+        private void b2_Click(object sender, EventArgs e)
+        {
+            pole.AppendText("2");
+        }
+
+        private void b0_Click(object sender, EventArgs e)
+        {
+            pole.AppendText("0");
+        }
+
+        private void b1_Click(object sender, EventArgs e)
+        {
+            pole.AppendText("1");
+        }
+
+        private void b4_Click(object sender, EventArgs e)
+        {
+            pole.AppendText("4");
+        }
+
+        private void b7_Click(object sender, EventArgs e)
+        {
+            pole.AppendText("7");
+        }
+
+        private void rowna_Click(object sender, EventArgs e)
+        {
+            switch (znak)
+            {
+                case '+':
+                    pole.Text = (liczba + double.Parse(pole.Text)).ToString();
+                    break;
+                case '-':
+                    pole.Text = (liczba - double.Parse(pole.Text)).ToString();
+                    break;
+                case '*':
+                    pole.Text = (liczba * double.Parse(pole.Text)).ToString();
+                    break;
+                case '/':
+                    if (pole.TextLength==1 && pole.Text[0].Equals('0'))
+                    {
+                        pole.Text = "Nie można dzielić przez zero!";
+                        break;
+                    }
+                    else
+                    {
+                        pole.Text = ((double)liczba / double.Parse(pole.Text)).ToString();
+                        break;
+                    }
+                default:
+                    break;
+            }
+        }
+
+        private void wyczysc_Click(object sender, EventArgs e)
+        {
+            pole.Text = "";
+        }
+
+        private void cofnij_Click(object sender, EventArgs e)
+        {
+            int textLength = pole.Text.Length;
+            if(textLength > 0)
+            {
+                pole.Text = pole.Text.Substring(0, textLength - 1);
+            }
+        }
+    }
+}
